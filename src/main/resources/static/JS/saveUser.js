296cd65f550be514724add042eb248daaa10bbb2
IronWil1 <aidarziyangirov@gmail.com>
2022-08-27 18:01:21 +0300

    Spring Rest + JS (JQuery)

--- src/main/resources/static/JS/saveUser.js
+++ src/main/resources/static/JS/saveUser.js
@@ -1,22 +1,4 @@
 urlSaveUser = 'http://localhost:8080/api/admin/save'
-
-async function _postData(url = '', data = {}) {
-    const response = await fetch(url, {
-        method: 'POST',
-        mode: 'cors',
-        cache: 'no-cache',
-        credentials: 'same-origin',
-        redirect: 'follow',
-        referrerPolicy: 'no-referrer',
-        headers: {
-            "Content-type": "application/json; charset=UTF-8"
-        },
-        body: JSON.stringify(data)
-    });
-
-    return response.json();
-}
-
 function handleSaveSubmit(event) {
     event.preventDefault();
     const data = new FormData(event.target);
